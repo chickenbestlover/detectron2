9205460b14afed9f98e6fdff443e59b640fbbbad
chickenbestlover <jmpark@rit.kaist.ac.kr>
2020-02-20 14:55:58 +0900

    add eval with GT masking

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -161,9 +161,9 @@ class GeneralizedRCNN(nn.Module):
         if "instances" in batched_inputs[0] and self.cfg.MODEL.KEEP_TARGET:
             gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
         elif "targets" in batched_inputs[0] and self.cfg.MODEL.KEEP_TARGET:
-            log_first_n(
-                logging.WARN, "'targets' in the model inputs is now renamed to 'instances'!", n=10
-            )
+            # log_first_n(
+            #     logging.WARN, "'targets' in the model inputs is now renamed to 'instances'!", n=10
+            # )
             gt_instances = [x["targets"].to(self.device) for x in batched_inputs]
         else:
             gt_instances = None
